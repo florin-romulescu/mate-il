7181a0bea52b35e117f52ee36966191804960068
Romulescu Florin Sorin <fromulescu@MacBook-Pro-Romulescu.local>
2024-07-21 13:00:16 +0300

    Schimbat nume de la date la created_at pentru consistenta

--- backend/mate_il/api/models.py
+++ backend/mate_il/api/models.py
@@ -5,7 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     description = models.TextField()
-    date = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     image = models.ImageField(upload_to='images/', blank=True, null=True)
     attachments = models.ManyToManyField('Attachment', blank=True)
     external_url = models.URLField(blank=True, null=True)
