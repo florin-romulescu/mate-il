1742a80f263308b05aaced7e79f41e02b68754a7
Romulescu Florin Sorin <fromulescu@MacBook-Pro-Romulescu.local>
2024-07-21 14:00:58 +0300

    Am schimbat numele in pk pentru a fi recunoscut de framework

--- backend/mate_il/api/urls.py
+++ backend/mate_il/api/urls.py
@@ -3,9 +3,9 @@ from .views import PostList, PostDetail, post_search, AnnounceList, AnnounceDeta
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
-    path('posts/<int:id>/', PostDetail.as_view(), name='post-detail'),
+    path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
     path('posts/search/', post_search, name='post-search'),
     path('announces/', AnnounceList.as_view(), name='announce-list'),
-    path('announces/<int:id>/', AnnounceDetail.as_view(), name='announce-detail'),
+    path('announces/<int:pk>/', AnnounceDetail.as_view(), name='announce-detail'),
     path('announces/search/', announce_search, name='announce-search'),
 ]
