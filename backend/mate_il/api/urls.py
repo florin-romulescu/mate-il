7c81b3102065d2b886a882d3bf258a0821096403
Romulescu Florin Sorin <fromulescu@MacBook-Pro-Romulescu.local>
2024-07-21 13:16:51 +0300

    Adaugare endpointuri

---
+++ backend/mate_il/api/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import PostList, PostDetail, post_search, AnnounceList, AnnounceDetail, announce_search
+
+urlpatterns = [
+    path('posts/', PostList.as_view(), name='post-list'),
+    path('posts/<int:id>/', PostDetail.as_view(), name='post-detail'),
+    path('posts/search/', post_search, name='post-search'),
+    path('announces/', AnnounceList.as_view(), name='announce-list'),
+    path('announces/<int:id>/', AnnounceDetail.as_view(), name='announce-detail'),
+    path('announces/search/', announce_search, name='announce-search'),
+]
