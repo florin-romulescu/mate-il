9682957d85c192cdd7000de54a4c99c8291bfe80
Romulescu Florin Sorin <fromulescu@MacBook-Pro-Romulescu.local>
2024-07-21 12:56:20 +0300

    Model Postare

--- backend/mate_il/api/models.py
+++ backend/mate_il/api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='images/', blank=True, null=True)
+    attachments = models.ManyToManyField('Attachment', blank=True)
+    external_url = models.URLField(blank=True, null=True)
